9735292a9455052a116dfe8e3c1e61e71dddbbbe
shravan <kshravankumarchary@gmail.com>
2026-03-22 17:14:37 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Result />
+      <BuyCredit />
+    </div>
+  );
+};
+
+export default App;
